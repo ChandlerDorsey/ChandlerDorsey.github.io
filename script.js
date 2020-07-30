f85279f6f22ee8e4b952bee8c65da57c8d18a807
ChandlerDorsey <thechandlerdorsey@gmail.com>
2020-07-30 17:31:40 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,12 @@
-const hamburger = document.querySelector(
-  ".header .nav-bar .nav-list .hamburger"
-);
+const menu = document.querySelector(".header .nav-bar .nav-list .menu");
 const mobile_menu = document.querySelector(".header .nav-bar .nav-list ul");
 const menu_item = document.querySelectorAll(
   ".header .nav-bar .nav-list ul li a"
 );
 const header = document.querySelector(".header.container");
 
-hamburger.addEventListener("click", () => {
-  hamburger.classList.toggle("active");
+menu.addEventListener("click", () => {
+  menu.classList.toggle("active");
   mobile_menu.classList.toggle("active");
 });
 
@@ -23,7 +21,7 @@ document.addEventListener("scroll", () => {
 
 menu_item.forEach((item) => {
   item.addEventListener("click", () => {
-    hamburger.classList.toggle("active");
+    menu.classList.toggle("active");
     mobile_menu.classList.toggle("active");
   });
 });
